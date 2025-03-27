eaf879f117d002bc5092cca563f5ea10bdf829c6
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-03-27 22:25:04 +0600

    google consent screen is showing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const passport = require('passport');
-const GoogleStrategy=require('passport-google-oauth20')
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -16,30 +16,39 @@ app.use(cors(corsOptions));
 app.use(express.json());
 // *passport related appis
 
-passport.use(new GoogleStrategy({
-    // options for google strategy
-    clientID:process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-}), ()=>{
-    // passport callback function
-}
-)
-
-
+passport.use(
+  new GoogleStrategy(
+    {
+      // options for google strategy
+      callbackURL: "/auth/google/redirect",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    () => {
+      // passport callback function
+    }
+  )
+);
 
 // auth logout
 
-app.get("/auth/logout",async(req,res)=>{
-    // handle with passport
-    res.send("logging out")
-})
-
+app.get("/auth/logout", async (req, res) => {
+  // handle with passport
+  res.send("logging out");
+});
 
 // auth with google
-app.get('/auth/google',async(req,res)=>{
-    // handle with passport
-    res.send("logging in with google")
-})
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile"],  //this will show google screen
+  })
+);
+
+// callback route for google to redirect to
+// app.get('/auth/google/redirect',async(req,res)=>{
+
+// })
 
 app.get("/", (req, res) => {
   res.send("oAuth 2.0 with passport server is running");
