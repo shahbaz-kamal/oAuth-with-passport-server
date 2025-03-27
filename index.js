6a5127336fa8d8dab881a025df7b40bd4eb01d85
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-03-27 21:53:09 +0600

    added passport with google auth

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const passport = require('passport');
+const GoogleStrategy=require('passport-google-oauth20')
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -12,8 +14,32 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+// *passport related appis
+
+passport.use(new GoogleStrategy({
+    // options for google strategy
+    clientID:process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+}), ()=>{
+    // passport callback function
+}
+)
 
 
+
+// auth logout
+
+app.get("/auth/logout",async(req,res)=>{
+    // handle with passport
+    res.send("logging out")
+})
+
+
+// auth with google
+app.get('/auth/google',async(req,res)=>{
+    // handle with passport
+    res.send("logging in with google")
+})
 
 app.get("/", (req, res) => {
   res.send("oAuth 2.0 with passport server is running");
